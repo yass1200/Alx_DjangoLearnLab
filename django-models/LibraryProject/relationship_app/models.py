e9669de8576a06b8ea9f0577d9120556e5eb1dd4
Yassine ASSOUALI <yassouali@gmail.com>
2025-11-10 01:27:03 +0100

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -25,3 +25,19 @@ class UserProfile(models.Model):
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     # Auto-create profile for new users; ensure it exists for existing users
     UserProfile.objects.get_or_create(user=instance)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
+
+    class Meta:
+        # Custom permissions (different from Django’s default add/change/delete)
+        permissions = (
+            ("can_add_book", "Can add book (custom)"),
+            ("can_change_book", "Can change book (custom)"),
+            ("can_delete_book", "Can delete book (custom)"),
+        )
