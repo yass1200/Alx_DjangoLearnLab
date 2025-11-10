7cb43ab8b749efdb3bb80c23ce214d239c06e4d9
Yassine ASSOUALI <yassouali@gmail.com>
2025-11-10 01:09:55 +0100

    Delete django-models/LibraryProject directory

--- django-models/LibraryProject/LibraryProject/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('', include('relationship_app.urls')),
-    path('admin/', admin.site.urls),
-]
