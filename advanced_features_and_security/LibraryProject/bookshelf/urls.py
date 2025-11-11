4f6ec1432bf15d59e1d1660ada21dbd971d23d03
Yassine ASSOUALI <yassouali@gmail.com>
2025-11-11 15:57:02 +0100

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,10 +2,14 @@
 from django.urls import path
 from . import views
 
+
+from django.urls import path
+from . import views
+
 app_name = "bookshelf"
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),
+    path("books/", views.book_list, name="book_list"),        # <- matches the checker
     path("books/add/", views.add_book, name="add_book"),
     path("books/<int:pk>/edit/", views.edit_book, name="edit_book"),
     path("books/<int:pk>/delete/", views.delete_book, name="delete_book"),
