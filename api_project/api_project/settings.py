eb23146ee2d78af8dadbdcd57c421657539828d4
Yassine ASSOUALI <yassouali@gmail.com>
2025-11-19 20:01:58 +0100

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',  # Add this
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
