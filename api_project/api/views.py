3cab67a457eba7a7ced6bc15666561457ae86d3f
Yassine ASSOUALI <yassouali@gmail.com>
2025-11-19 19:59:48 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,3 +7,8 @@ from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+# Add the new ViewSet for full CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
