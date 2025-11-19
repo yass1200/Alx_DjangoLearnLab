da18ee16ff77382f7e358097b1710fa6dcaf2551
Yassine ASSOUALI <yassouali@gmail.com>
2025-11-19 20:04:00 +0100

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -9,9 +9,13 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Token authentication endpoint
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    
     # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
     
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),
 ]
+
