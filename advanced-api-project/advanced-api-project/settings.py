a14b2f89d81130d180fd90d1d94ff3fbfe50684c
Yassine ASSOUALI <yassouali@gmail.com>
2025-11-29 19:47:45 +0100

    Update settings.py

--- advanced-api-project/advanced-api-project/settings.py
+++ advanced-api-project/advanced-api-project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
     'api',  # Add this
 ]
 
@@ -121,3 +122,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
