09082127edc2a0bc776280a58a4aed51ce1abd14
Yassine ASSOUALI <yassouali@gmail.com>
2025-12-06 18:32:52 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -8,14 +8,8 @@ from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)  # CRITICAL
-    
-    def __str__(self):
-        return self.title
+from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -25,9 +19,6 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
-    
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
@@ -39,3 +30,4 @@ class Comment(models.Model):
     def __str__(self):
 
         return f'Comment by {self.author} on {self.post}'
+
