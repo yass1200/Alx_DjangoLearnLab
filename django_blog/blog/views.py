082eda64f26a2ee3fc99f130ae3f6ee876d47c1c
Yassine ASSOUALI <yassouali@gmail.com>
2025-12-06 18:26:38 +0100

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Post
 
-# Create your views here.
+def home_view(request):
+    return render(request, 'blog/home.html')
+
+def posts_view(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/posts.html', {'posts': posts})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
