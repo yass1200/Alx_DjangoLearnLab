4360f97c58bc93777a09458a26b39c0c9e8d6836
Yassine ASSOUALI <yassouali@gmail.com>
2025-12-06 18:34:51 +0100

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -11,7 +11,18 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.auth.models import User
 
+"""
+Models for Django Blog
+"""
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class Post(models.Model):
+    """
+    Blog post model
+    """
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
@@ -19,6 +30,30 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Profile(models.Model):
+    """
+    User profile model extending Django's User
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+# Signal to create profile when user is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
@@ -30,4 +65,5 @@ class Comment(models.Model):
     def __str__(self):
 
         return f'Comment by {self.author} on {self.post}'
+
 
