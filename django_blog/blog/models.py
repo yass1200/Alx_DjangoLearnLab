29845a58ba22d6acc6dfba4b06f6c5c810a47fcf
Yassine ASSOUALI <yassouali@gmail.com>
2025-12-06 17:32:18 +0100

    Add files via upload

---
+++ django_blog/blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved_comment = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
---
+++ django_blog/static/js/main.js
@@ -0,0 +1,15 @@
+// static/js/main.js
+// Add any interactive functionality here
+console.log('Django Blog loaded');
+
+// Confirm before deleting
+document.addEventListener('DOMContentLoaded', function() {
+    const deleteButtons = document.querySelectorAll('.delete-btn');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function(e) {
+            if (!confirm('Are you sure you want to delete this?')) {
+                e.preventDefault();
+            }
+        });
+    });
+});
