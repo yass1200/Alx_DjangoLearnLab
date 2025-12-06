a2371b74be87c9953a7b314bcfc98e38d91f69d4
Yassine ASSOUALI <yassouali@gmail.com>
2025-12-06 18:26:00 +0100

    Create urls.py

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('posts/', views.posts_view, name='posts'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('register/', views.register_view, name='register'),
+]
