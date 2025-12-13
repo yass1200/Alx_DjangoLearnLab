5acf7d128debc6b4e621d4d9c31d4fac72fb904f
Yassine ASSOUALI <yassouali@gmail.com>
2025-12-13 15:45:52 +0100

    Delete social_media_api directory

--- social_media_api/notifications/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Notification
-
-class NotificationSerializer(serializers.ModelSerializer):
-    actor_username = serializers.ReadOnlyField(source="actor.username")
-
-    class Meta:
-        model = Notification
-        fields = ("id", "actor_username", "verb", "timestamp", "is_read")
